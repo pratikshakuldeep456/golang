3e5451a9109bb64d0531163d2c0ec303d4733936
pratikshakuldeep456 <pratikshakuldeep464440@gmail.com>
2025-07-15 15:56:56 +0530

    golang practice

---
+++ notes/chan.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Sendchan(ping chan<- string, m string) {
+	ping <- m
+}
+func Recchan(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+func main1() {
+	fmt.Println("Hello, 世界")
+	ping := make(chan string, 1)
+	pong := make(chan string, 1)
+	Sendchan(ping, "kkd")
+	Recchan(ping, pong)
+	data := <-pong
+	fmt.Println(data)
+}
